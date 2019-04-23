b4c61ee99d4e658922bf0121cf315830e4d0faa1
IfanTsai <1558499691@qq.com>
2019-04-23 17:40:38 +0800

    格式调整

--- source/at24c02.c
+++ source/at24c02.c
@@ -11,24 +11,24 @@ static void delay100ms()
 
 void at24c02_write(u8 addr, u8 dat)
 {
-	i2c_start();
-	i2c_send_byte(0xa0);  // 发送器件地址 （最低位为0表示写）
-	i2c_send_byte(addr);  // 发送要写入内存地址
-	i2c_send_byte(dat);   // 发送要写入的数据
+    i2c_start();
+    i2c_send_byte(0xa0);  // 发送器件地址 （最低位为0表示写）
+    i2c_send_byte(addr);  // 发送要写入内存地址
+    i2c_send_byte(dat);   // 发送要写入的数据
     i2c_stop();
     delay100ms();
 }
 
 u8 at24c02_read(u8 addr)
 {
-	u8 num;
-	i2c_start();
-	i2c_send_byte(0xa0);    // 发送写器件地址
-	i2c_send_byte(addr);    // 发送要读取的地址
-	i2c_start();
-	i2c_send_byte(0xa1);    // 发送器件地址 （最低位为1表示读）
-	num = i2c_recv_byte();  // 读取数据
-	i2c_stop();
-	delay100ms();
-	return num;
+    u8 num;
+    i2c_start();
+    i2c_send_byte(0xa0);    // 发送写器件地址
+    i2c_send_byte(addr);    // 发送要读取的地址
+    i2c_start();
+    i2c_send_byte(0xa1);    // 发送器件地址 （最低位为1表示读）
+    num = i2c_recv_byte();  // 读取数据
+    i2c_stop();
+    delay100ms();
+    return num;
 }
