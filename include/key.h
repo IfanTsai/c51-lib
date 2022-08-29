fc787aee160e2c1c6d4f3354106c40ee7ceedabe
Pokoai <pokeusr@outlook.com>
2022-08-29 16:39:21 +0800

    '函数返回类型由uchar改为char'

--- include/key.h
+++ include/key.h
@@ -5,13 +5,13 @@
  * module：4*4矩阵按键
  * author：IfanTsai
  * api：
- * uchar key_polling()   轮询模式读取4*4按键，成功返回0 - 15，失败返回-1
- * uchar key_blocking()  阻塞模式读取4*4按键，成功返回0 - 15，失败返回-1
+ * char key_polling()   轮询模式读取4*4按键，成功返回0 - 15，失败返回-1
+ * char key_blocking()  阻塞模式读取4*4按键，成功返回0 - 15，失败返回-1
  */
 
 #include "main.h"
 
-uchar key_polling();
-uchar key_blocking();
+char key_polling();
+char key_blocking();
 
 #endif
